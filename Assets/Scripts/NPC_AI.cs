282cea7b5cbece1bf937bf538a1df0fb47bd3549
José Magalhães <joses.magalhaes@hotmail.com>
2026-03-10 19:58:55 -0300

    :construction: Add a enemy with pathfinding ai

--- Assets/Scripts/NPC_AI.cs
+++ Assets/Scripts/NPC_AI.cs
@@ -6,19 +6,36 @@ public class NPC_AI : MonoBehaviour
 {
     public List<Transform> wayPoints;
     private UnityEngine.AI.NavMeshAgent navMeshAgent;
+    private Animator anim;
 
     public int currentWaipointIndex = 0;
     public float speed = 2f;
+    public bool isPlayerDetected = false;
+
+    private bool onRadius;
+    private bool hasAttacked = false;
+
     void Start()
    {
         navMeshAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         navMeshAgent.speed = speed;
+        anim = GetComponent<Animator>();
    }
 
 
     void Update()
     {
-        Walking();
+        if (!isPlayerDetected)
+        {
+            Walking();
+        }
+        else
+        {
+            StopWalking();
+            anim.ResetTrigger("Attack");
+            anim.SetTrigger("Attack");
+            hasAttacked = true;
+        }
     }
 
     private void Walking()
@@ -36,7 +53,36 @@ public class NPC_AI : MonoBehaviour
             currentWaipointIndex = (currentWaipointIndex + 1) % wayPoints.Count;
         }
 
+        navMeshAgent.isStopped = false;
         navMeshAgent.SetDestination(wayPoints[currentWaipointIndex].position);
+        onRadius = false;
 
     }
+
+    private void StopWalking()
+    {
+        onRadius = true;
+        navMeshAgent.isStopped = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")){
+            Debug.Log("Player Detectado!");
+            isPlayerDetected = true;
+            hasAttacked = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player saiu do raio de interação!");
+            isPlayerDetected = false;
+            navMeshAgent.isStopped = false;
+            anim.ResetTrigger("Attack");
+        }
+    }
+
 }
